208d6fa7059d2d82ceaa9f3e5a891805c0086050
bhargavav <venkatabhargavit@gmail.com>
2023-11-21 23:48:09 +0530

    Access all the elements in the interface.
    This commit introduces accessing all the UI elements on the screen and using an onclick listener on Calculate button which displays a toast message after we click on it.

--- app/src/main/java/com/AppTechInnovationLab/bmicalculator/MainActivity.java
+++ app/src/main/java/com/AppTechInnovationLab/bmicalculator/MainActivity.java
@@ -3,6 +3,12 @@ package com.AppTechInnovationLab.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RadioButton maleRadioButton = findViewById(R.id.radio_button_male);
+        RadioButton femaleRadioButton = findViewById(R.id.radio_button_female);
+        EditText ageEditText = findViewById(R.id.edit_text_age);
+        EditText feetEditText = findViewById(R.id.edit_text_feet);
+        EditText inchesEditText = findViewById(R.id.edit_text_inches);
+        EditText weightEditText = findViewById(R.id.edit_text_weight);
+        Button calculateButton = findViewById(R.id.button_calculate);
+        TextView resultTextView = findViewById(R.id.text_view_result);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button-I am working!!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }

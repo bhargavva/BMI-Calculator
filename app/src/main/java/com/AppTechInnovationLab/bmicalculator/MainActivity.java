d6290b8f23a75c87cad3d1d1d3217e3f12590b8a
bhargavav <venkatabhargavit@gmail.com>
2023-11-22 20:27:40 +0530

    Add functionality to calculate BMI.
    This commit introduces BMI calculation, here we took input values form the appropriate views and change the values from text to int and we calculate the BMI using the BMI formula.

--- app/src/main/java/com/AppTechInnovationLab/bmicalculator/MainActivity.java
+++ app/src/main/java/com/AppTechInnovationLab/bmicalculator/MainActivity.java
@@ -12,26 +12,67 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RadioButton maleRadioButton;
+    private RadioButton femaleRadioButton;
+    private EditText ageEditText;
+    private EditText feetEditText;
+    private EditText inchesEditText;
+    private EditText weightEditText;
+    private Button calculateButton;
+    private TextView resultTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getViews();
+        setButtonClickListener();
 
-        RadioButton maleRadioButton = findViewById(R.id.radio_button_male);
-        RadioButton femaleRadioButton = findViewById(R.id.radio_button_female);
-        EditText ageEditText = findViewById(R.id.edit_text_age);
-        EditText feetEditText = findViewById(R.id.edit_text_feet);
-        EditText inchesEditText = findViewById(R.id.edit_text_inches);
-        EditText weightEditText = findViewById(R.id.edit_text_weight);
-        Button calculateButton = findViewById(R.id.button_calculate);
-        TextView resultTextView = findViewById(R.id.text_view_result);
+    }
 
+    private void getViews() {
+        maleRadioButton = findViewById(R.id.radio_button_male);
+        femaleRadioButton = findViewById(R.id.radio_button_female);
+        ageEditText = findViewById(R.id.edit_text_age);
+        feetEditText = findViewById(R.id.edit_text_feet);
+        inchesEditText = findViewById(R.id.edit_text_inches);
+        weightEditText = findViewById(R.id.edit_text_weight);
+        calculateButton = findViewById(R.id.button_calculate);
+        resultTextView = findViewById(R.id.text_view_result);
+    }
+
+    private void setButtonClickListener() {
         calculateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Button-I am working!!!", Toast.LENGTH_SHORT).show();
+                calculateBMI();
+                //Toast.makeText(MainActivity.this, "Button-I am working!!!", Toast.LENGTH_SHORT).show();
             }
         });
+    }
 
+    private void calculateBMI() {
+        String ageText = ageEditText.getText().toString();
+        String feetText = feetEditText.getText().toString();
+        String inchesText = inchesEditText.getText().toString();
+        String weightText = weightEditText.getText().toString();
+
+        int age = Integer.parseInt(ageText);
+        int feet = Integer.parseInt(feetText);
+        int inches = Integer.parseInt(inchesText);
+        int weight = Integer.parseInt(weightText);
+
+        //Convert feet to inches.
+        int totalInches = (inches * 12) + inches;
+
+        //Convert total inches to meters
+        double heightInMeters = totalInches * 0.0254;
+
+        //Calculate BMI
+        double BMI = weight / (heightInMeters * heightInMeters);
+
+        //Convert BMI to text
+        String bmiText = String.valueOf(BMI);
+        resultTextView.setText(bmiText);
     }
 }
